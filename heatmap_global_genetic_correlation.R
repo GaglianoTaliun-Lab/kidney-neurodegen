22ff19ab46506a92f036b9ff614d4ed1a1ac2bd3
Sadaf Gawhary <sadafgawhary4@gmail.com>
2025-02-26 14:12:44 -0500

    Update heatmap_global_genetic_correlation.R

--- heatmap_global_genetic_correlation.R
+++ heatmap_global_genetic_correlation.R
@@ -6,15 +6,95 @@ library(pheatmap)
 library(reshape2)
 library(grid)  
 
+# Figure 2 : heatmap AD/PD and PD meta-analyzed with and no proxy 
+# Create data frame
+data <- data.frame(
+  Disease = c("AD_sexcombined", "AD_sexcombined", "AD_sexcombined", "AD_sexcombined", "AD_sexcombined", "AD_sexcombined",
+              "NP_AD_sexcombined", "NP_AD_sexcombined", "NP_AD_sexcombined", "NP_AD_sexcombined", "NP_AD_sexcombined", "NP_AD_sexcombined", 
+              "PD_sexcombined", "PD_sexcombined", "PD_sexcombined", "PD_sexcombined", "PD_sexcombined", "PD_sexcombined",
+              "NP_PD_sexcombined", "NP_PD_sexcombined", "NP_PD_sexcombined", "NP_PD_sexcombined", "NP_PD_sexcombined", "NP_PD_sexcombined",
+              "PD_sexcombined_meta", "PD_sexcombined_meta", "PD_sexcombined_meta", "PD_sexcombined_meta", "PD_sexcombined_meta", "PD_sexcombined_meta",
+              "NP_PD_sexcombined_meta", "NP_PD_sexcombined_meta", "NP_PD_sexcombined_meta", "NP_PD_sexcombined_meta", "NP_PD_sexcombined_meta", "NP_PD_sexcombined_meta"),
+  Traits = c("Hematuria", "Microalbumin", "Creatinine", "Potassium", "Sodium", "eGFR",
+             "Hematuria", "Microalbumin", "Creatinine", "Potassium", "Sodium", "eGFR",
+             "Hematuria", "Microalbumin", "Creatinine", "Potassium", "Sodium", "eGFR",
+             "Hematuria", "Microalbumin", "Creatinine", "Potassium", "Sodium", "eGFR",
+             "Hematuria", "Microalbumin", "Creatinine", "Potassium", "Sodium", "eGFR",
+             "Hematuria", "Microalbumin", "Creatinine", "Potassium", "Sodium", "eGFR"),
+  Rg = c(0.1702, 0.0401, -0.0651, -0.1156, -0.0245, 0.0222,
+         -0.1668, -0.0293, 0.022, -0.0408, 0.1092, 0.075,
+         -0.1218, -0.0873, -0.1124, -0.0915, -0.1238, 0.0236,
+         -0.1160, -0.1027, -0.0871, -0.1062, -0.0726, 0.0262,
+         -0.1542, -0.1149, -0.1561, -0.0789, -0.1672, 0.0627,
+         -0.1728, -0.1033, -0.1369, -0.0769, -0.1513, 0.0742),
+  P_value = c(0.1444, 0.7217, 0.2199, 0.0763, 0.6343, 0.6867,
+              0.1057, 0.8025, 0.7123, 0.5358, 0.0678, 0.109,
+              0.0991, 0.2253, 0.0014, 0.0208, 0.0009, 0.4872,
+              0.1501, 0.1376, 0.0115, 0.0066, 0.0545, 0.419,
+              0.0219, 0.1483, 0.00000089915, 0.0515, 0.00000053694, 0.0735,
+              0.011, 0.2532, 0.000086083, 0.0827, 0.000010169, 0.0464))
+
+# Remove rows with missing Rg values
+data <- data[!is.na(data$Rg), ]
+
+# Add significance asterisks based on P_value
+data$Significance <- ifelse(data$P_value < 0.008, "***", 
+                            ifelse(data$P_value < 0.01, "**", 
+                                   ifelse(data$P_value < 0.05, "*", "")))
+
+# Reshape the data into a matrix for pheatmap
+heatmap_data <- reshape2::acast(data, Traits ~ Disease, value.var = "Rg")
+significance_data <- acast(data, Traits ~ Disease, value.var = "Significance")
+
+# Reorder the traits and diseases
+ordered_traits <- c("eGFR", "Hematuria", "Creatinine", "Microalbumin", "Potassium", "Sodium")
+ordered_diseases <- c("AD_sexcombined", "NP_AD_sexcombined", "PD_sexcombined", "NP_PD_sexcombined", "PD_sexcombined_meta", "NP_PD_sexcombined_meta")
+
+# Ensure columns are in the correct order
+heatmap_data <- heatmap_data[ordered_traits, ordered_diseases]
+significance_data <- significance_data[ordered_traits, ordered_diseases]
+
+# Define a color palette with 0 as white, negative values as blue, and positive as red
+color_palette <- colorRampPalette(c("blue", "white", "red"))(100)
+
+fixed_range <- 0.3
+
+custom_display <- function(values, significance) {
+  # Combine the values and significance annotations
+  formatted_data <- matrix(sprintf("%.3f%s", values, significance), 
+                           nrow = nrow(values), 
+                           dimnames = dimnames(values))
+  return(formatted_data)
+}
+
+
+pheatmap(heatmap_data, 
+         cluster_rows = FALSE, 
+         cluster_cols = FALSE, 
+         display_numbers = custom_display(heatmap_data, significance_data), 
+         color = color_palette, 
+         number_color = "black",
+         breaks = seq(-fixed_range, fixed_range, length.out = 101), 
+         labels_col = c("Proxy", "No Proxy", "Proxy", "No Proxy", "Proxy", "No Proxy"),
+         angle_col = 0, 
+         gaps_col = c(2,4),
+         main = " ",
+         fontsize_number = 10)
+
+
+# Fig sup 1. Heatmap of AD and PD with proxies
 # Create the dataframe for AD/PD heatmap
 data <- data.frame(
   Disease = c("AD_sexcombined", "AD_sexcombined", "AD_sexcombined", "AD_sexcombined", "AD_sexcombined", "AD_sexcombined",
               "AD_male", "AD_male", "AD_male", "AD_male", "AD_male", "AD_male",
               "AD_female", "AD_female", "AD_female", "AD_female", "AD_female", "AD_female",
               "PD_sexcombined", "PD_sexcombined", "PD_sexcombined", "PD_sexcombined", "PD_sexcombined", "PD_sexcombined",
+              "PD_sexcombined_meta", "PD_sexcombined_meta", "PD_sexcombined_meta", "PD_sexcombined_meta", "PD_sexcombined_meta", "PD_sexcombined_meta",
+              "PD_male", "PD_male", "PD_male", "PD_male", "PD_male", "PD_male",
               "PD_male", "PD_male", "PD_male", "PD_male", "PD_male", "PD_male",
               "PD_female", "PD_female", "PD_female", "PD_female", "PD_female", "PD_female"),
   Traits = c("Hematuria", "Microalbumin", "Creatinine", "Potassium", "Sodium", "eGFR",
+             "Hematuria", "Microalbumin", "Creatinine", "Potassium", "Sodium", "eGFR",
              "Hematuria", "Microalbumin", "Creatinine", "Potassium", "Sodium", "eGFR",
              "Hematuria", "Microalbumin", "Creatinine", "Potassium", "Sodium", "eGFR",
              "Hematuria", "Microalbumin", "Creatinine", "Potassium", "Sodium", "eGFR",
@@ -24,12 +104,14 @@ data <- data.frame(
          -0.1443, -0.2499, -0.0995, -0.216, 0.0408, 0.0158,
          0.0624, -0.2769, 0.0034, -0.0705, -0.1937, 0.0059,
          -0.1218, -0.0873, -0.1124, -0.0915, -0.1238, 0.0236,
+         -0.1542, -0.1149, -0.1561, -0.0789, -0.1672, 0.0627,
          -0.1959, -0.1173, -0.0601, 0.0725, -0.0878, 0.012,
          -0.3245, -0.1497, -0.2061, -0.1495, -0.1869, 0.0668),
   P_value = c(0.1444, 0.7217, 0.2199, 0.0763, 0.6343, 0.6867,
               0.6121, 0.4076, 0.4931, 0.2463, 0.7852, 0.9005,
               0.7705, 0.3427, 0.9714, 0.53305, 0.0757, 0.94,
               0.0991, 0.2253, 0.0014, 0.0208, 0.0009, 0.4872,
+              0.0219, 0.1483, 0.00000089915, 0.0515, 0.00000053694, 0.0735,
               0.1071, 0.2959, 0.2312, 0.2891, 0.0909, 0.8002,
               0.0056, 0.4406, 0.0003, 0.0077, 0.0004, 0.213)
 )
@@ -48,7 +130,7 @@ significance_data <- acast(data, Traits ~ Disease, value.var = "Significance")
 
 # Reorder the traits and diseases
 ordered_traits <- c("eGFR", "Hematuria", "Creatinine", "Microalbumin", "Potassium", "Sodium")
-ordered_diseases <- c("AD_sexcombined", "AD_male", "AD_female", "PD_sexcombined", "PD_male", "PD_female")
+ordered_diseases <- c("AD_sexcombined", "AD_male", "AD_female", "PD_sexcombined", "PD_sexcombined_meta", "PD_male", "PD_female")
 
 # Ensure columns are in the correct order
 heatmap_data <- heatmap_data[ordered_traits, ordered_diseases]
@@ -74,30 +156,37 @@ pheatmap(heatmap_data,
          breaks = seq(-fixed_range, fixed_range, length.out = 101), 
          number_color = "black",
          gaps_col = c(3),
-         labels_col = c("Sex-\ncombined", "Male", "Female", "Sex-\ncombined", "Male", "Female"),  #Change the labels for different data used
+         labels_col = c("Sex-\ncombined", "Male", "Female", "Sex-\ncombined", "Sex-\ncombined\nmeta-analyzed", "Male", "Female"),  #Change the labels for different data used
          main = " ",
          angle_col = 0,
          fontsize_number = 10)
 
-### Heatmap for PD with and No proxy
+
+### Fig 3. Heatmap for PD with and No proxy
 
 # Create the data frame for PD and No Proxy PD
 data <- data.frame(
   Disease = c("PD_sexcombined", "PD_sexcombined", "PD_sexcombined", "PD_sexcombined", "PD_sexcombined", "PD_sexcombined",
               "NP_PD_sexcombined", "NP_PD_sexcombined", "NP_PD_sexcombined", "NP_PD_sexcombined", "NP_PD_sexcombined", "NP_PD_sexcombined",
+              "PD_sexcombined_meta", "PD_sexcombined_meta", "PD_sexcombined_meta", "PD_sexcombined_meta", "PD_sexcombined_meta", "PD_sexcombined_meta",
+              "NP_PD_sexcombined_meta", "NP_PD_sexcombined_meta", "NP_PD_sexcombined_meta", "NP_PD_sexcombined_meta", "NP_PD_sexcombined_meta", "NP_PD_sexcombined_meta",
               "PD_male", "PD_male", "PD_male", "PD_male", "PD_male", "PD_male",
               "NP_PD_male", "NP_PD_male", "NP_PD_male", "NP_PD_male", "NP_PD_male", "NP_PD_male",
               "PD_female", "PD_female", "PD_female", "PD_female", "PD_female", "PD_female",
               "NP_PD_female", "NP_PD_female", "NP_PD_female", "NP_PD_female", "NP_PD_female", "NP_PD_female"),
-  Traits = c(rep(c("Creatinine", "eGFR", "Hematuria", "Microalbumin", "Potassium", "Sodium"), 6)),
+  Traits = c(rep(c("Creatinine", "eGFR", "Hematuria", "Microalbumin", "Potassium", "Sodium"), 8)),
   Rg = c(-0.1124, 0.0236, -0.1218, -0.0873, -0.0915, -0.1238,  # PD_sexcombined
          -0.0871, 0.0262, -0.1160, -0.1027, -0.1062, -0.0726,  # No_proxy_PD_sexcombined
+         -0.1561, 0.0627, -0.1543, -0.1149, -0.0789, -0.1672,  # PD_sexcombined_meta_analyzed
+         -0.1369, 0.0742, -0.1728, -0.1033, -0.0769, -0.1513,   # NP_PD_sexcombined_meta_analyzed
          -0.0601, 0.012, -0.1959, -0.1173, 0.0725, -0.0878,  # PD_male
          -0.0466, -0.0003, -0.1681, -0.0573, 0.0557, -0.1197,  # No_proxy_PD_male
          -0.2061, 0.0668, -0.3245, -0.1497, -0.1495, -0.1869,  # PD_female
          -0.2125, 0.0955, -0.3343, -0.2015, -0.1214, -0.2013),  # No_proxy_PD_female
   P_value = c(0.0014, 0.4872, 0.0991, 0.2253, 0.0208, 0.0009,  # PD_sexcombined
               0.0115, 0.419, 0.1501, 0.1376, 0.0066, 0.0545,  # No_proxy_PD_sexcombined
+              0.00000089915, 0.0735, 0.0219, 0.1483, 0.0515, 0.00000053694, # PD_sexcombined_meta_analyzed 
+              0.000086083, 0.0464, 0.011, 0.2532, 0.0827, 0.000010169,   # NP_PD_sexcombined_meta_analyzed 
               0.2312, 0.8002, 0.1071, 0.2959, 0.2891, 0.0909,  # PD_male
               0.3881, 0.995, 0.1742, 0.6188, 0.4405, 0.0326,  # No_proxy_PD_male
               0.0003, 0.213, 0.0056, 0.4406, 0.0077, 0.0004,  # PD_female
@@ -118,7 +207,7 @@ significance_data <- acast(data, Traits ~ Disease, value.var = "Significance")
 
 # Reorder the diseases and traits in the desired order
 ordered_traits <- c("eGFR", "Hematuria", "Creatinine", "Microalbumin", "Potassium", "Sodium")
-ordered_diseases <- c("PD_sexcombined", "NP_PD_sexcombined", "PD_male", "NP_PD_male", "PD_female", "NP_PD_female")
+ordered_diseases <- c("PD_sexcombined", "NP_PD_sexcombined", "PD_sexcombined_meta", "NP_PD_sexcombined_meta", "PD_male", "NP_PD_male", "PD_female", "NP_PD_female")
 
 # Ensure columns are in the correct order
 heatmap_data <- heatmap_data[ordered_traits, ordered_diseases]
@@ -146,7 +235,8 @@ pheatmap(heatmap_data,
          breaks = seq(-fixed_range, fixed_range, length.out = 101), 
          fontsize_number = 10,
          number_color = "black",  # Set number color to black
-         labels_col = c("Proxy", "No Proxy", "Proxy", "No Proxy", "Proxy", "No Proxy"), # Add the custom labels for columns
+         labels_col = c("Proxy", "No Proxy", "Proxy", "No Proxy", "Proxy", "No Proxy", "Proxy", "No Proxy"), # Add the custom labels for columns
          angle_col = 0,
+         gaps_col = c(2,4,6),
          main = " "
 )
